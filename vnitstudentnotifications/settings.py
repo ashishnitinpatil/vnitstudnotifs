66f8047e5fd558f518e1a1cc3a4eacd27ab10ac6
Ashish Patil <ashishnitinpatil@gmail.com>
2014-08-13 03:11:31 +0530

    +syncdb, +django admin static

--- vnitstudentnotifications/settings.py
+++ vnitstudentnotifications/settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -124,9 +124,9 @@ INSTALLED_APPS = (
     'autoload',
     'dbindexer',
 
+    'vnitstudentnotifications.coreapp',
     # djangoappengine should come last, so it can override a few manage.py commands
     'djangoappengine',
-    'vnitstudentnotifications.coreapp',
 )
 
 # A sample logging configuration. The only tangible logging
